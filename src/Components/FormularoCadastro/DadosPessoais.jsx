08b7c05e21cbfb0c1740442d0bb5f9169280a2f8
Cristiano Fonseca <ckfonseca@gmail.com>
2022-03-08 17:38:43 -0300

    Hooks validation included.

--- src/Components/FormularoCadastro/DadosPessoais.jsx
+++ src/Components/FormularoCadastro/DadosPessoais.jsx
@@ -1,14 +1,16 @@
 import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import ValidacoesCadasro from "../../Contextos/ValidacoesCadastro";
 
-function DadosPessoais({ aoEnviar, validacoes }) {
+function DadosPessoais({ aoEnviar }) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(true);
   const [erros, setErros] = useState({ cpf: { valido: true, texto: "" }, nome:{ valido: true, texto: "" } });
-
+  const validacoes = useContext(ValidacoesCadasro);
+  
   function validarCampos(e) {
     const { name, value } = e.target;
     const novoEstado = { ...erros };
---
+++ src/Contextos/ValidacoesCadastro.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ValidacoesCadasro = React.createContext();
+
+export default ValidacoesCadasro;
